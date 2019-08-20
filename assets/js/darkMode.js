aca0a7f24edd5317e033f598499e9c0ea9930316
Marc <marcjfj@gmail.com>
2019-08-20 10:56:30 -0500

    added dark mode

---
+++ assets/js/darkMode.js
@@ -0,0 +1,26 @@
+// adds a "dark" class to the body
+// in style.css I've added a set of rules for when .dark is applied
+// I'm using cookies so that the dark/light preference persists across pages
+
+let darkToggle = document.querySelector(".dark-mode-toggle");
+
+darkToggle.addEventListener("click", () =>{
+    if (document.cookie != "dark=true"){
+        document.cookie = "dark=true; path=/";
+        darkToggle.textContent = "Light Mode";
+    }else{
+        document.cookie = "dark=true; expires=Thu, 01 Jan 1970 00:00:00; path=/";
+        darkToggle.textContent = "Dark Mode";
+    }
+    checkDark();
+});
+
+let checkDark = () => {
+    if (document.cookie == "dark=true"){
+        document.body.classList.add("dark");
+    }else{
+        document.body.classList.remove("dark");
+    }
+}
+
+checkDark();
